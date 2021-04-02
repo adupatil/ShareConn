107ddba0886a236bcc4704322b33b1b692fff13f
Sameer Mahajan <sam241099@gmail.com>
2021-04-02 16:35:29 +0530

    Social Login functionality added

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
-from .viewsets import UserViewSet,UserFollowViewSet,UserProfileViewSet,LogoutExView
+from .viewsets import UserViewSet,UserFollowViewSet,UserProfileViewSet,LogoutExView, FacebookLogin
 from rest_framework import routers
-from django.urls import path, include   
+from django.urls import path, include
+from django.conf.urls import url 
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register('api/users',UserViewSet)
@@ -9,7 +11,9 @@ router.register('api/users_profile',UserProfileViewSet)
 
 urlpatterns = [ 
     path('', include(router.urls)),
-    path('rest-auth/logout',LogoutExView.as_view(),name='rest-logout')
+    path('rest-auth/logout',LogoutExView.as_view(),name='rest-logout'),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
+    path('fb/', TemplateView.as_view(template_name="fb.html")),
 ]
 
 
