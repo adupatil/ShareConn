0e3cfe1da0ce03c6f3a2039bb76d612e9d704e0c
Adwait Patil <adwaitpatil1199@gmail.com>
2021-04-06 13:15:17 +0530

    tried parsers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,3 +31,5 @@ urlpatterns = [
     path(r'^', include('django.contrib.auth.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/posts/viewsets.py
+++ backend/posts/viewsets.py
@@ -2,14 +2,28 @@ from rest_framework import viewsets
 from .serializers import PostSerializer,LikeSerializer,CommentSerializer
 from .models import Post,Like,Comment
 from rest_framework.permissions import IsAuthenticated
-
+#from django.http import HttpResponse
+from rest_framework.parsers import FormParser,MultiPartParser
 
 class PostViewset(viewsets.ModelViewSet):
     permission_classes=[IsAuthenticated]
+    parser_classes = [FormParser,MultiPartParser]
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    # def post(self, request, *args, **kwargs):
+    #     post_title = request.data['post_title']
+    #     post_type = request.data['post_type']
+    #     category = request.data['category']
+    #     post_text = request.data['post_text']
+    #     user_id = request.data['user_id']
+
+    #     Post.objects.create(post_title=post_title,post_type=post_type,category=category,post_text=post_text,
+    #     user_id=user_id)
+
+    #     return HttpResponse({"message": "Post created",
+    #     "status":200})
 class LikeViewset(viewsets.ModelViewSet):
     permission_classes=[IsAuthenticated]
 
