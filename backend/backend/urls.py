a9c5525ea3b2450f0b8adffb7133312939ea029d
Adwait Patil <adwaitpatil1199@gmail.com>
2021-04-07 12:18:36 +0530

    settings updated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url 
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -32,4 +34,7 @@ urlpatterns = [
     url(r'^accounts/',include('allauth.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
